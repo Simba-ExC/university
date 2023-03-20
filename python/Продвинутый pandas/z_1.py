80a691183ac4aa15d42547c424f2474086da30e2
Simba-ExC <117112169+Simba-ExC@users.noreply.github.com>
2023-03-20 20:05:20 +0300

    Add files via upload

---
+++ python/Продвинутый pandas/z_1.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+
+data = pd.read_csv('visit_log.csv',delimiter='')
+print
+sort = data.query("traffic_source == @yandex or traffic_source == @google")
+print (sort)
+
+
+#data = pd.read_csv('keywords.csv', delimiter=',') # записываем из файла в DataFrame
+#data.insert(2, "region", "unidentified")
+#entry = 0           # вспомогательная переменная для 
+#
+#geo_data = {
+#'Центр': ['москва', 'тула', 'ярославль'],
+#'Северо-Запад': ['петербург', 'псков', 'мурманск'],
+#'Дальний Восток': ['владивосток', 'сахалин', 'хабаровск']
+#}
+#for r in range (3):
+#    for c in range (2):
+#        name = list(geo_data.items())[r][c]
+#        sort = data.query("keyword == @name")
+#        for r in range (len(sort)):
+#            row1 = sort.iloc[[r]]
+#           row2 = row1.iloc[0].values
+#            print ("есть вхождение в строке : ", row2,"добавим",list(geo_data.items())[r][0])
+#           data.loc[data['shows'] == row2[r], 'region' ] = list(geo_data.items())[r][0]
+
