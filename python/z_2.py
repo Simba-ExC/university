74113abfee9ed333406c3dd052fa924ee5d5d5a3
Simba-ExC <117112169+Simba-ExC@users.noreply.github.com>
2023-03-20 20:04:32 +0300

    Delete z_2.py

--- python/z_2.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-data = pd.read_csv('power.csv')
-
-data.query('quantity > "1" & country == "Latvian" | country == "Lithuania" | country == "Estonia" & year > "2005" & year < "2010"  ')
-
-print(data)
